6afaeaef68c9bb1f6ab95374bd1f8e5e45ef062e
Vincent M. van der Meulen <meulen@uw.edu>
2017-10-12 15:44:11 -0700

    Add first test

--- lab01-testing/test_handlers/handlers_test.go
+++ lab01-testing/test_handlers/handlers_test.go
@@ -1,7 +1,36 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
 func TestComplimentHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/compliments", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	name := "steve"
+	q := req.URL.Query()
+	q.Add("name", name)
+	req.URL.RawQuery = q.Encode()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ComplimentHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned the wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expectedPrefix := fmt.Sprintf("%s, you are", name)
+	if !strings.HasPrefix(rr.Body.String(), expectedPrefix) {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expectedPrefix)
+	}
 }
