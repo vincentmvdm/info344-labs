d1b94bf6ce726e5eccb1d23ef6689f6ecc55e767
aethan <aethan@uw.edu>
2017-10-12 14:37:37 -0700

    Finish writing up handlers test code

--- lab01-testing/test_handlers/handlers.go
+++ lab01-testing/test_handlers/handlers.go
@@ -7,17 +7,35 @@ import (
 	"time"
 )
 
-type ComplimentHandler struct {
-	Compliments []string
-}
+// type ComplimentHandler struct {
+// 	Compliments []string
+// }
 
-func (ch *ComplimentHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// func (ch *ComplimentHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Add("Access-Control-Allow-Origin", "*")
+// 	name := r.FormValue("name")
+
+// 	// randomly select a compliment from the Compliments
+// 	rand.Seed(time.Now().Unix())
+// 	compliment := ch.Compliments[rand.Intn(len(ch.Compliments))]
+// 	fmt.Fprintf(w, "%s, you are %s", name, compliment)
+
+// }
+
+func ComplimentHandler(w http.ResponseWriter, r *http.Request) {
+	compliments := []string{"wonderful", "strong",
+		"beautiful", "smart", "loyal", "friendly",
+		"good", "asynchronous", "savvy", "handsome",
+		"meme-y", "athletic", "intelligent", "good at Golang",
+		"creative", "groundbreaking", "artistic", "suave",
+		"capable", "bodacious"}
+
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	name := r.FormValue("name")
 
 	// randomly select a compliment from the Compliments
 	rand.Seed(time.Now().Unix())
-	compliment := ch.Compliments[rand.Intn(len(ch.Compliments))]
+	compliment := compliments[rand.Intn(len(compliments))]
 	fmt.Fprintf(w, "%s, you are %s", name, compliment)
 
 }
--- lab01-testing/test_handlers/handlers_test.go
+++ lab01-testing/test_handlers/handlers_test.go
@@ -1,7 +1,46 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
 func TestComplimentHandler(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	// e.g. GET /api/projects?page=1&per_page=100
+	req, err := http.NewRequest("GET", "/compliments", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	// modify the url query to add the name query parameters
+	name := "ethan"
+	q := req.URL.Query()
+	q.Add("name", name)
+	req.URL.RawQuery = q.Encode() // re encode the query string
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ComplimentHandler)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expectedPrefix := fmt.Sprintf("%s, you are", name)
+	if !strings.HasPrefix(rr.Body.String(), expectedPrefix) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expectedPrefix)
+	}
 }
